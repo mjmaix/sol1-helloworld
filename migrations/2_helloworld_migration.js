69c08dd7e60dfabe65164227cfb232ab32265cb3
MJ <mjmaix@gmail.com>
2022-03-06 00:53:55 +0800

    added ropsten from moralis
    install pretter
    
    hide ropsten network from moralis

---
+++ migrations/2_helloworld_migration.js
@@ -0,0 +1,10 @@
+const HelloWorld = artifacts.require('HelloWorld')
+const HelloWorldPermanent = artifacts.require('HelloWorldPermanent')
+
+module.exports = function (deployer) {
+    deployer.deploy(HelloWorld, 'Hello world constructor').then(async () => {
+        let instance = await HelloWorld.deployed()
+        let message = instance.hello()
+        return deployer.deploy(HelloWorldPermanent, message)
+    })
+}
