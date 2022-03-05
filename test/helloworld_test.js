da39b29d69b87a9b983ba7416f0b54f8c097fa91
MJ <mjmaix@gmail.com>
2022-03-06 01:17:46 +0800

    install pretter

--- test/helloworld_test.js
+++ test/helloworld_test.js
@@ -1,27 +1,27 @@
-const HelloWorld = artifacts.require("HelloWorld");
+const HelloWorld = artifacts.require('HelloWorld')
 
-contract("HelloWorld", (accounts) => {
-  it("constructor should set the message correctly", async () => {
-    let instance = await HelloWorld.deployed();
-    let message = await instance.message();
+contract('HelloWorld', (accounts) => {
+    it('constructor should set the message correctly', async () => {
+        let instance = await HelloWorld.deployed()
+        let message = await instance.message()
 
-    assert.equal(message, "Hello world constructor");
-  });
+        assert.equal(message, 'Hello world constructor')
+    })
 
-  it("owner should be accounts[0]", async () => {
-    let instance = await HelloWorld.deployed();
-    let owner = await instance.owner();
+    it('owner should be accounts[0]', async () => {
+        let instance = await HelloWorld.deployed()
+        let owner = await instance.owner()
 
-    assert.equal(accounts[0], owner);
-  });
+        assert.equal(accounts[0], owner)
+    })
 
-  it("setMessage should change the message", async () => {
-    const newMessage = "I was changed";
-    let instance = await HelloWorld.deployed();
+    it('setMessage should change the message', async () => {
+        const newMessage = 'I was changed'
+        let instance = await HelloWorld.deployed()
 
-    await instance.setMessage(newMessage);
-    let message = await instance.message();
+        await instance.setMessage(newMessage)
+        let message = await instance.message()
 
-    assert.equal(newMessage, message);
-  });
-});
+        assert.equal(newMessage, message)
+    })
+})
