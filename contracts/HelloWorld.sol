4ffa26ce5aca540a9273a48a2be972645db554ba
MJ <mjmaix@gmail.com>
2022-03-06 00:10:30 +0800

    format on save

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,7 +2,6 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorld {
-
     string public message;
     address public owner;
 
--- contracts/HelloWorldPermanent.sol
+++ contracts/HelloWorldPermanent.sol
@@ -2,13 +2,9 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract HelloWorldPermanent {
-
     string public message;
 
     constructor(string memory _message) {
         message = _message;
-       
     }
-
-
 }
